c3deec63ead4f0473dba78bede408f482c815288
ulhuqabrar <ulhuqabrar001@gmail.com>
2026-07-26 16:25:32 +0530

    auth change

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,8 +1,9 @@
-import { createAuthClient } from 'better-auth/react';
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
+  baseURL: "https://brandcora-api.onrender.com",
   fetchOptions: {
-    credentials: 'include',
+    credentials: "include",
   },
 });
 
